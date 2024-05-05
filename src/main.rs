e3b3faf95efb0b852e0b8453137f41af9e7ac6e3
Amit Singh Sethi <amit.pureenergy@gmail.com>
2024-05-05 17:08:32 +0530

    write to stream in rust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
- use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,8 +14,11 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut buf: [u8; 512] = [0; 512];
+                _stream.read(&mut buf).unwrap();
+                _stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
